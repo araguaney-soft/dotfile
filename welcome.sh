fe5073b195af98635529cd2a280b977f9f0ac38e
raanfefu <rafael.fernandez.ve@gmail.com>
2022-01-10 22:54:53 -0500

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Welcome libs scripts for dev-containers"
+echo "***********************************************"
+echo "*** Welcome libs scripts for dev-containers ***"
+echo "***********************************************"
+
+echo 'copying scripts to /usr/bin"
+
+sudo cp .dotfiles/* /usr/bin/ 
+
